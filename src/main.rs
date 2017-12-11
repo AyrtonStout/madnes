c74096ac2922decaa333d6b01468b3a75545304c
Ayrton Stout <AyrtonStout@outlook.com>
2017-12-10 17:56:42 -0700

    Added code that will read bytes from a ROM and print out individual bytes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
 	println!("Hello World!");
-	dude();
+	read_file();
 }
 
-fn dude() {
+fn read_file() {
 	println!("Wat");
+    let mut buffer = vec![0; 10];
+//    let mut file = File::open("Super Mario Bros 3 (E).nes").expect("Bad things");
+    let mut file = File::open("Contra (USA).nes").expect("Bad things");
+
+    file.read_to_end(&mut buffer).expect("More bad things");
+
+    print!("{}", buffer[10]); // This is the first 'N' in 'N' 'E' 'S' 'EOF'
 }
 
 #[cfg(test)]
