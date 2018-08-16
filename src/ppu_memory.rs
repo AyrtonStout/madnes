ce09fe4a6e2112e5c19c707ad89fe7485a07099b
AyrtonStout <AyrtonStout@outlook.com>
2018-08-15 23:19:40 -0600

    Added support for memory mirroring for the PPU

--- src/ppu_memory.rs
+++ src/ppu_memory.rs
@@ -26,69 +26,91 @@ impl PPUMemory {
         }
     }
 
-    pub fn get_8_bit_value(&self, address: u16) -> u8 {
+    pub fn get_8_bit_value(&self, mut address: u16) -> u8 {
+        address = self.get_non_mirrored_address(address);
         return self.memory[address as usize];
     }
 
-    pub fn get_16_bit_value(&self, address: u16) -> u16 {
+    pub fn get_16_bit_value(&self, mut address: u16) -> u16 {
+        address = self.get_non_mirrored_address(address);
+
         // Little Endian. Low byte is stored first
         let high_byte: u16 = (self.memory[address as usize + 1] as u16) << 8;
         let low_byte: u16 = self.memory[address as usize] as u16;
         return high_byte | low_byte;
     }
 
-    pub fn get_memory_range(&self, address: u16, num_bytes: u16) -> Vec<u8> {
+    pub fn get_memory_range(&self, mut address: u16, num_bytes: u16) -> Vec<u8> {
+        address = self.get_non_mirrored_address(address);
+
         let memory: &[u8] = &self.memory[(address as usize)..(address as usize + num_bytes as usize)];
         let mut memory_copy: Vec<u8> = vec![0; num_bytes as usize];
         memory_copy.copy_from_slice(memory);
         return memory_copy;
     }
 
-    pub fn set_8_bit_value(&mut self, address: u16, value: u8) {
-        PPUMemory::check_valid_write(address);
+    pub fn set_8_bit_value(&mut self, mut address: u16, value: u8) {
+        address = self.get_non_mirrored_address(address);
 
         self.memory[address as usize] = value;
     }
 
-    pub fn set_16_bit_value(&mut self, address: u16, value: u16) {
-        PPUMemory::check_valid_write(address);
+    pub fn set_16_bit_value(&mut self, mut address: u16, value: u16) {
+        address = self.get_non_mirrored_address(address);
 
         self.memory[address as usize] = value as u8;
         self.memory[address as usize + 1] = (value >> 8) as u8;
     }
 
-    fn check_valid_write(address: u16) {
-        if address >= 0x8000 {
-            panic!("Attempting to write to read-only memory address: {}!", address);
+    // The PPU addresses more space than it can actually use. When operating on certain memory values, it is
+    // functionally equivalent to be operating on a different value instead (writing to 0x4000 is the same as writing to 0x0)
+    fn get_non_mirrored_address(&self, mut address: u16) -> u16 {
+        if address >= 0x4000 {
+            address %= 0x4000;
+        }
+
+        if address >= 0x3000 && address <= 0x3EFF {
+            return address - 0x1000;
+        } else if address >= 0x3F20 && address <= 0x3FFF {
+            let mini_address = address % 0x20; // This is a shitty name but I can't think of a better one right now
+            return 0x3F00 + mini_address;
+        } else {
+            return address;
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use cpu_memory::CPUMemory;
+    use ppu_memory::PPUMemory;
 
     #[test]
     fn can_get_a_stored_8_bit_value() {
-        let mut memory: CPUMemory = CPUMemory::new();
+        let mut memory = PPUMemory::new();
         memory.set_8_bit_value(0x1500, 150);
         assert_eq!(memory.get_8_bit_value(0x1500), 150);
+
+        memory.set_8_bit_value(0x4500, 150);
+        assert_eq!(memory.get_8_bit_value(0x500), 150);
     }
 
     #[test]
     fn can_get_a_stored_16_bit_value() {
-        let mut memory: CPUMemory = CPUMemory::new();
+        let mut memory = PPUMemory::new();
 
         memory.set_16_bit_value(0x1500, 150); // 8 bit value stored as 16
         assert_eq!(memory.get_16_bit_value(0x1500), 150);
 
         memory.set_16_bit_value(0x1500, 10450); // 16 bit value also stored as 16
         assert_eq!(memory.get_16_bit_value(0x1500), 10450);
+
+        memory.set_16_bit_value(0x4500, 150); // 8 bit value stored as 16
+        assert_eq!(memory.get_16_bit_value(0x500), 150);
     }
 
     #[test]
     fn can_store_two_8_bit_values_and_read_back_as_a_16_bit_value() {
-        let mut memory: CPUMemory = CPUMemory::new();
+        let mut memory = PPUMemory::new();
 
         memory.set_8_bit_value(0x1500, 0x42); // 8 bit value stored as 16
         memory.set_8_bit_value(0x1501, 0xA5); // 16 bit value also stored as 16
